86e96728398c9104f4c79d5a30998ca1499f330e
barganakukuhraditya <barganakukuh@gmail.com>
2024-04-26 15:07:32 +0700

    new function

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
-import { get } from "https://bukulapak.github.io/api/process.js"; 
-let urlAPI = "https://ws-bargana2024-abecd99cf108.herokuapp.com/presensi";
+import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
+let urlAPI = "https://ws-indra2024-878f7e6fab92.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
